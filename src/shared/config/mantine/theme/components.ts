a36e3de23e4beac72dac4a4a9b30b727486614d9
shaadcode <shaadcode@gmail.com>
2026-09-23 13:14:48 +0330

    fix: Next.js error due to missing `height` and `width` attributes

--- src/shared/config/mantine/theme/components.ts
+++ src/shared/config/mantine/theme/components.ts
@@ -7,5 +7,13 @@ import { ClientLink } from '@/shared/lib/nextExtended'
 export const components = {
   Anchor: Anchor.extend({ defaultProps: { component: ClientLink } }),
   NavLink: NavLink.extend({ defaultProps: { component: ClientLink } }),
-  Image: MantineImage.extend({ defaultProps: { component: Image } }),
+  Image: MantineImage.extend({
+    defaultProps: {
+      component: Image,
+      // These values are mandatory in the Next.js Image component. Use `w` and `h`.
+      // @ts-expect-error
+      width: 100,
+      height: 100,
+    },
+  }),
 } as const satisfies Parameters<typeof createTheme>[0]['components']
